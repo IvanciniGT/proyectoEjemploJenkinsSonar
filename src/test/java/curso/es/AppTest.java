1022bd9e51f31ea9c39206608fe22d0ec5ed6be8
Ubuntu <ubuntu@ip-172-31-41-198.eu-west-1.compute.internal>
2024-01-18 09:53:14 +0000

    proyecto

---
+++ src/test/java/curso/es/AppTest.java
@@ -0,0 +1,28 @@
+package curso.es;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void sumarNumerosTest() {
+        App miApp = new App();
+        int numero1 = 5;
+        int numero2 = 10;
+        int resultado = miApp.sumarNumeros(numero1, numero2);
+        int resultadoEsperado = 15;
+        assertEquals( resultadoEsperado, resultado );
+    }
+    
+    @Test
+    public void mainTest() {
+        try{
+            App.main(null);
+        }catch(Exception e){
+            fail("No se debe lanzar exception al ejecutar este código");
+        }
+    }
+}
